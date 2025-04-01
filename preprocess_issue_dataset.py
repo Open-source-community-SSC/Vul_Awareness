ff9d0aa7946c3a26a95bccbf1a97603f80a19516
Clyly <112835425+Clyly@users.noreply.github.com>
2025-04-01 13:17:15 +0800

    Update preprocess_issue_dataset.py

--- preprocess_issue_dataset.py
+++ preprocess_issue_dataset.py
@@ -10,14 +10,23 @@ def preprocess_issue_dataset(owner, repository):
     with open(f"issues/{owner}_{repository}_issues.json", 'r', encoding='utf-8') as f:
         issue_data = json.load(f)
 
-    dataset = pd.DataFrame([
-        {
-            "issue_id": issue["url"].split('/')[-1].strip(),
-            "title": issue["title"],
-            "body": issue["body"]
-        }
-        for issue in issue_data
-    ])
+    data_list = []
+
+    for issue in issue_data:
+        issue_id = issue["url"].split('/')[-1].strip()
+        title = issue["title"]
+        body = issue["body"]
+
+        data_list.append({
+            "issue_id": issue_id,
+            "title": title,
+            "body": body if body is not None else ""
+        })
+
+    dataset = pd.concat([
+        pd.DataFrame(data_list),
+        pd.DataFrame(columns=['issue_id', 'title', 'body'])
+    ], ignore_index=True)
 
     print("begin process...")
     dataset["title"] = dataset["title"].map(replace_tokens_simple_1)
@@ -25,6 +34,9 @@ def preprocess_issue_dataset(owner, repository):
     print("finish process...")
 
     dataset.to_csv(f"data/dataset_{owner}_{repository}_issue.csv", index=False)
+    with open(f"data/dataset_{owner}_{repository}_issue.json", 'w', encoding='utf-8') as f:
+        json.dump(data_list, f, ensure_ascii=False, indent=4)
+    logging.info(f"成功处理并保存 {owner}/{repository} 的 issue report 数据")
 
 if __name__ == "__main__":
     owner = "torvalds"
