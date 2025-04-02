1179331b9d836ae10539d30fe5cf8ba625dc7530
Clyly <112835425+Clyly@users.noreply.github.com>
2025-04-02 10:20:14 +0800

    Update run_model_issue_binary.py

--- run_model_issue_binary.py
+++ run_model_issue_binary.py
@@ -4,8 +4,6 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset
 from tqdm import tqdm
-best_f1_path = "model_best/mode_issue_binary_f1.txt"
-best_model_path = "model_best/model_issue_binary_model.pth"
 
 # Load pre-trained tokenizers
 tokenizer1 = AutoTokenizer.from_pretrained("./codebert_model")
@@ -15,7 +13,6 @@ labels = {'positive': 1, 'negative': 0}
 class Binary_Issue_Dataset(Dataset):
     def __init__(self, dataframe):
         self.texts = [tokenizer2(text, padding='max_length', max_length=512, truncation=True, return_tensors="pt") for text in dataframe['text']]
-        self.labels = [labels[label] for label in dataframe['flag']]
 
     def __len__(self):
         return len(self.texts)
@@ -32,8 +29,7 @@ class Binary_Issue_Dataset(Dataset):
 class Binary_Issue_Classifier(nn.Module):
     def __init__(self):
         super(Binary_Issue_Classifier, self).__init__()
-        self.bert1 = AutoModelForSequenceClassification.from_pretrained("mycodebert")
-        self.bert2 = BertModel.from_pretrained("mybert")
+        self.bert = BertModel.from_pretrained("bert_model")
         self.dropout = 0.3
         self.hidden = nn.Sequential(
             nn.Linear(768, 256),
@@ -50,15 +46,14 @@ class Binary_Issue_Classifier(nn.Module):
             nn.Dropout(self.dropout)
         )
 
-
     def forward(self, text, mask):
-        pooled_output1 = self.bert2(input_ids=text, attention_mask=mask, return_dict=False)[0]
+        pooled_output1 = self.bert(input_ids=text, attention_mask=mask, return_dict=False)[0]
         output_text = pooled_output1[:, 0, :]
         # need new compare method
         output = self.hidden(output_text)
         return output
 
-def run_model_issue_binary(dataset_PATH):
+def run_model_issue_binary(dataset_PATH, best_model_path="parameters/issue_binary_best_model.pth"):
     model = Binary_Issue_Classifier()
     df = pd.read_csv(dataset_PATH)
     dataset = Binary_Issue_Dataset(df)
@@ -70,9 +65,6 @@ def run_model_issue_binary(dataset_PATH):
 
     print("Now loading the best model parameters...")
     try:
-        with open(best_f1_path, 'r') as f:
-            best_f1_score = float(f.read())
-            f.close()
         model.load_state_dict(torch.load(best_model_path))
         print("Read best model successfully. Starting training...")
     except:
@@ -98,3 +90,6 @@ def run_model_issue_binary(dataset_PATH):
 
     return all_preds
 
+if __name__ == "__main__":
+    all_preds = run_model_issue_binary("data/dataset_torvalds_linux_issue.csv")
+    print(all_preds)
